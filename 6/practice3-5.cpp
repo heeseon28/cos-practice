f8deeeb30d3bd6e88f624ede3d31c7f8586c81dd
HEESEON <hgbell28@kentech.ac.kr>
2026-04-16 01:45:20 +0000

    7주차 목요일 실습

---
+++ 6/practice3-5.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+extern "C" int decode1(long* x, long* y, long* z);
+
+int main(){
+    long x, y, z;
+    x = 10000; y = 20000; z = 30000;
+    cout << "before decode1(): " << "x: " << x << " y: " << y << " z: " << z << endl;
+    decode1(&x, &y, &z);
+    cout << "after decode1(): " << "x: " << x << " y: " << y << " z: " << z << endl;
+    return 0;
+}
