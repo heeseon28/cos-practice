0124de82af918083999eec6d3a521e59d1edb7a3
heeseon28 <hgbell28@gmail.com>
2026-06-04 20:02:53 +0900

    feat::14주차 실습

---
+++ 7/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "human.h"
+using namespace std;
+
+int main()
+{
+    Human *hw;
+    hw = new Human("Hyunwoo Lee");
+    hw->print();
+    hw->setAge(37);
+    hw->print();
+}
---
+++ 8/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "human.h"
+using namespace std;
+
+int main()
+{
+    cout << "h1 >>>" << endl;
+    Human *h1 = new Student("Alice", 20);
+    cout << "h2 >>>" << endl;
+    Student *h2 = new Student("Bob", 20);
+    cout << "h3 >>>" << endl;
+    Human *h3 = new Human("Carol", 20);
+    h1->print();
+    h2->print();
+    h3->print();
+    return 0;
+}
